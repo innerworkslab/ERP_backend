511d7df1273c82dff00504bb8c95039acd15f508
zin win htun <zinwinhtun71@gmail.com>
2026-04-21 10:09:48 +0630

    fix: product image update bugs fix

--- Modules/Product/app/Http/Requests/Product/UpdateProduct.php
+++ Modules/Product/app/Http/Requests/Product/UpdateProduct.php
@@ -14,8 +14,6 @@ class UpdateProduct extends FormRequest
             'name' => 'required|string|max:255',
             'sku' => 'required|string|max:255|unique:products,sku,' . $id,
             'image' => 'nullable|file|image|mimes:jpg,jpeg,png,webp|max:5120',
-            'image_path' => 'prohibited',
-            'image_url' => 'prohibited',
             'category_id' => 'required|integer|exists:categories,id',
             'brand_id' => 'required|integer|exists:brands,id',
             'alert_quantity' => 'nullable|numeric|min:0',
