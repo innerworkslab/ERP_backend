4b5b451ccf7a4c58449368e41fcff191ec97b1dc
zin win htun <zinwinhtun71@gmail.com>
2026-06-18 10:14:27 +0630

    Modify:stock balance update

--- Modules/Inventory/app/Http/Requests/StockBalance/ListingRequest.php
+++ Modules/Inventory/app/Http/Requests/StockBalance/ListingRequest.php
@@ -15,6 +15,8 @@ class ListingRequest extends FormRequest
             'per_page' => 'nullable|integer|min:1|max:100',
             'page' => 'nullable|integer|min:1',
             'search' => 'nullable|string|max:255',
+            'date_from' => 'nullable|date',
+            'date_to' => 'nullable|date|after_or_equal:date_from',
             'branch_id' => 'nullable|integer|exists:branches,id',
             'inventory_id' => 'nullable|integer|exists:inventories,id',
             'product_id' => 'nullable|integer|exists:products,id',
